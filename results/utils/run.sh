dd3f4b7c6cbaaee9cc7502976f029f1ec115f899
Lorenzo Ruotolo <ruotololorenzo@gmail.com>
2024-01-16 13:24:37 +0100

    added run script

---
+++ results/utils/run.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+ROOT_DIR="/home/s313207/cv32e40p_ft/"
+
+SAF_LIST="./run/zoix/cv32e40p_top_saf.rpt"
+REPORT_FILE="./results/hardened/report.rpt"
+RUN_RPT_FILE="./run/zoix/cv32e40p_top_saf.rpt.fsim"
+
+
+## INIT ##
+
+cd $ROOT_DIR
+
+# Functions to print colored messages
+print_green() {
+    local message=$1
+    echo -e "\e[1;32m${message}\e[0m"
+}
+print_red() {
+    local message=$1
+    echo -e "\e[1;31m${message}\e[0m"
+}
+
+## RUN ##
+
+# SBST
+print_green "\nStarting SBST Compilation"
+make compile_sbst
+
+
+# Check for the -nosyn argument
+if [ "$1" != "-nosyn" ]; then
+    # Synthesis
+    print_green "\nStarting Synthesis"
+    make synthesis/nangate45
+fi
+
+# Logic Simulation
+print_green "\nStarting Logic Simulation"
+make questa/compile
+make questa/lsim/gate/shell
+zoix_lsim_output=$(make zoix/lsim 2>&1)
+if ! echo "$zoix_lsim_output" | grep -q "Info:    VCD stimulus completed with 0 mismatches."; then
+    print_red "\n[ERROR] Mismatches found in the logic simulation."
+    exit 1
+fi
+
+# Fault Simulation
+print_green "\nStarting Fault Simulation"
+make zoix/fgen/saf
+make zoix/fsim FAULT_LIST=$SAF_LIST
+
+# Generate Report
+print_green "\nGenerating Report"
+grep '^#' "$RUN_RPT_FILE" > "$REPORT_FILE"
+
+# Print Relevant Results
+awk '/Total Faults/ { faults=$NF } /Fault Coverage/ { coverage=$(NF-1) } END { printf "• Number of faults: \033[1;35m%s\033[0m\n", faults; printf "• Test coverage: \033[1;35m%s\033[0m\n", coverage }' "$REPORT_FILE"
+print_green "\n[DONE]\n"
